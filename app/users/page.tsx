7cdd49acb77137eddda98c9943c4a8c4014172d4
Sami <sradwan93@gmail.com>
2025-02-01 21:37:23 -0500

    added a sidebar and mobile footer

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+import EmptyState from "../components/EmptyState";
+
+const Users = () => {
+  return (
+    <div>
+      <div className="hidden lg:block lg:pl-80 h-[100vh]">
+        <EmptyState />
+      </div>
+    </div>
+  )
+}
+
+export default Users
